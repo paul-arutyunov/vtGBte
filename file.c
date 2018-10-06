c2a2e73fa8088e9d8628d88bde88c32371684f57
Paul_Arutyunov <pashaarutyunov@gmail.com>
2018-10-06 14:33:19 +0300

    Add file I/O

---
+++ file.c
@@ -0,0 +1,44 @@
+#include "file.h"
+
+FILE *fp;
+
+int initFile(char *filename)
+{
+	extern FILE *fp;
+	extern int h,w;
+	fp = fopen(filename,"rb+");
+	move(h+7,9);
+	if (fp!=NULL) {
+		printw("Opened file '%s'",filename);
+		return SUCCESS;	
+	} else {
+		printw("Can't open file '%s'",filename);
+		return FAILURE;
+	}
+}
+
+void saveAsset()
+{
+	extern unsigned char asset[256][16];
+	extern FILE *fp;
+	int i,j;
+
+	for (i=0; i<256; i++)
+	{
+		fwrite(asset[i],sizeof(unsigned char),16,fp);
+	}
+	fclose(fp);
+}
+
+void loadAsset()
+{
+	extern unsigned char asset[256][16];
+	extern FILE *fp;
+	int i,j;
+
+	for (i=0; i<256; i++)
+	{
+		fread(asset[i],sizeof(unsigned char),16,fp);
+	}
+	fclose(fp);
+}
---
+++ file.h
@@ -0,0 +1,12 @@
+#ifndef _FILE_H_
+#define _FILE_H_
+
+#include "def.h"
+#include "stdio.h"
+#include "ncurses.h"
+
+int initFile(char *filename);
+void saveAsset();
+void loadAsset();
+
+#endif
