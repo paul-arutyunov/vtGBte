ec44a5c876b07b4bbafab4576fab5ed80a540b0d
Paul_Arutyunov <pashaarutyunov@gmail.com>
2018-11-01 18:54:50 +0300

    Add an option to create file if it doesn't exist

--- src/file.c
+++ src/file.c
@@ -6,16 +6,33 @@ int initFile(char *filename)
 {
 	extern FILE *fp;
 	extern int h,w;
+	int c;
 	fp = fopen(filename,"rb+");
 	move(h+7,9);
 	if (fp!=NULL) {
 		printw("Opened file '%s'",filename);
 		return SUCCESS;	
 	} else {
-	/*	fp = fopen(filename,"wb
-		printw("Created file '%s'",filename);*/
-		printw("Can't open file '%s'",filename);
-		return FAILURE;
+		printw("There's no file '%s'. Create?",filename);
+		c=getch();
+
+		if (c=='y')
+		{
+			fp = fopen(filename,"wb+");
+
+			if (fp==NULL) {
+				printw("\r         Can't create file '%s'          ",filename);
+				return FAILURE;
+			} else {
+				printw("\r         Created file '%s'               ",filename);
+				return SUCCESS;
+			}
+		}
+		if (c=='n')
+		{
+			printw("\r        Oki.                 ");
+			return FAILURE;
+		}
 	}
 }
 
